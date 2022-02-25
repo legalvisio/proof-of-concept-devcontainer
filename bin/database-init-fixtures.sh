a782616533777d76ecf42cfcfd0f59392a9767e6
legaldeveloper <52700307+legaldeveloper@users.noreply.github.com>
2022-02-25 17:36:02 +0000

    have to wait database init?

--- bin/database-init-fixtures.sh
+++ bin/database-init-fixtures.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+pwd
+sleep 30
 psql postgresql://postgres:postgres@database:5432/postgres < fixtures/structure.sql
 psql postgresql://postgres:postgres@database:5432/postgres < fixtures/demo-data.sql
